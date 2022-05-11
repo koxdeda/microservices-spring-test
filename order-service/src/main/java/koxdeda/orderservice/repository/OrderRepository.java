22f6ed051d67358c1b1e7c20979e889b3fb6e86c
koxdeda <kox.deda@yandex.ru>
2022-05-12 00:30:30 +0300

    Initial commit - created a product, order and inventory services

---
+++ order-service/src/main/java/koxdeda/orderservice/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package koxdeda.orderservice.repository;
+
+import koxdeda.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ product-service/src/main/java/koxdeda/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package koxdeda.productservice.repository;
+
+import koxdeda.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
